a311773780e45c9b71c9b18ef419376e8db56720
jucora <artjulius@hotmail.com>
2019-11-19 12:39:11 -0500

    updating the Users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,14 @@ class UsersController < ApplicationController
 
 	def new
 		@user =User.new
-  	end
+  end
 
-  	def create
+  def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
